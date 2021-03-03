b6a959945a3da454c102756c65ab88040b89f7b1
madhusudan sahu <madhusudan.sahu@sap.com>
2021-03-03 23:19:06 +0530

    first commit

---
+++ packages/calculate/index.js
@@ -0,0 +1,6 @@
+
+var addition = require('@mysm/addition');
+var substration = require('substration');
+
+console.log("add(4+5)"+addition.add(4,5));
+console.log("substract(6-5)"+substration.substract(6,5));
