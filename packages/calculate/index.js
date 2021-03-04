af9454ec6669f791321f52e8e2b348bac11e4579
madhusudan sahu <madhusudan.sahu@sap.com>
2021-03-04 08:43:19 +0530

    test

--- packages/calculate/index.js
+++ packages/calculate/index.js
@@ -3,6 +3,6 @@ var addition = require('@mysm/addition');
 var substration = require('substration');
 // testing..
 
-//test again.
+//test again.....
 console.log("add(4+5)"+addition.add(4,5));
 console.log("substract(6-5)"+substration.substract(6,5));
